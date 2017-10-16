bb78930ddff8ccf99a74981fe0d8bcd1d9d4da58
nuoyakang <kangnuoya@gmail.com>
2017-10-16 14:53:52 -0700

    src/R/analysis and markdown 10/16 final commit

--- src/R/analysis/casestudy1.r
+++ src/R/analysis/casestudy1.r
@@ -1,7 +1,10 @@
 #read the raw data of brewery#
 brewery <- read.csv("C:/Users/Dell Eater/Nuoya's/SMU Nuoya/MSDS6306/CaseStudy1/data/raw/Breweries.csv", sep = ',')
-#find the unique values in brewery
-numberBrewery <- unique(brewery)
+
+#find out how many breweries are present in each state
+eachstate <- table(factor(brewery$State))
+eachstate <- data.frame(eachstate)
+eachstate
 
 #read the raw data of beer data set
 beer <- read.csv("C:/Users/Dell Eater/Nuoya's/SMU Nuoya/MSDS6306/CaseStudy1/data/raw/Beers.csv", sep = ',')
@@ -14,21 +17,23 @@ mergebeer <- mergebeer[c(3,2,6,4,5,7,1,8,9,10)]
 colnames(mergebeer)[2] <- 'Beer Name'
 colnames(mergebeer)[8] <- 'Brewery Name'
 
+#find the first 6 and last 6
+first6 <- head(mergebeer,6)
+first6
+last6 <- tail(mergebeer,6)
+last6
 #write the merged data to the processed file
 write.csv(mergebeer,"C:/Users/Dell Eater/Nuoya's/SMU Nuoya/MSDS6306/CaseStudy1/data/processed/merged.csv", row.names = FALSE)
 
-#find the first 6 and last 6
-first6 <- head(mergebeer,6)
-last6 <- tail(mergebeer,6)
-
-#find the NA for each column
 numberNA <- colSums(is.na(mergebeer))
+numberNA <- data.frame(numberNA)
 numberNA
 
 #subset the data as state,ABV and IBU
 subsetbeer<- data.frame(mergebeer$State,
 												mergebeer$ABV,
 												mergebeer$IBU)
+
 #apply the function to find the median
 MedianABV <- tapply(subsetbeer$mergebeer.ABV,subsetbeer$mergebeer.State,median)
 MedianIBU <- tapply(subsetbeer$mergebeer.IBU,subsetbeer$mergebeer.State,median)
@@ -45,7 +50,7 @@ finalmedian <- finalmedian[c(3,1,2)]
 rownames(finalmedian) <- NULL
 finalmedian
 
-#draw the bar plot
+#draw the bar plot pf abv
 par(mar=c(18, 4.1, 4.1, 2.1), mgp=c(3, 1, 0), las=2) #http://rfunction.com/archives/1302
 
 bp <- barplot(finalmedian$`Median ABV`,
@@ -53,6 +58,18 @@ bp <- barplot(finalmedian$`Median ABV`,
 							xlab  ='Median Alcohol Content',
 							ylim = c(0,55),
 							col = 'blue',
+							horiz=T, 
+							density = 50,
+							las = 1)
+
+#draw the bar plot pf IBU
+par(mar=c(18, 4.1, 4.1, 2.1), mgp=c(3, 1, 0), las=2) #http://rfunction.com/archives/1302
+
+bp <- barplot(finalmedian$`Median IBU`,
+							ylab ='States',
+							xlab  ='Median Alcohol Content',
+							ylim = c(0,55),
+							col = 'red',
 							horiz=T, 
 							density = 50,
 							las = 1)
